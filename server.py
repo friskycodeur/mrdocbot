ad799fe783aa5ef4b7f87a95e8efe90939522384
Prateek <45481137+moodyarrow@users.noreply.github.com>
2020-04-03 23:25:41 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,24 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+#Here we will import the chat function from main.py
 from main import chat
 
-
+#connection to server
 class ChatServer(WebSocket):
 
     def handleMessage(self):
         # echo message back to client
         message = self.data
+        #Get the written message by user and give it as input to chat()
         response = chat(message)
+        #Print the response of from the chat()
         self.sendMessage(response)
 
     def handleConnected(self):
+        #print connected when connected to server
         print(self.address, 'connected')
 
     def handleClose(self):
+        #print closed when the server is closed
         print(self.address, 'closed')
 
 
